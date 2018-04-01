3d603cbf0a90566bc71043dfd65afd947dad02f2
baby7305 <songhaibei7305@163.com>
2018-04-01 14:35:58 +0800

    工程初始化

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace webclient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
